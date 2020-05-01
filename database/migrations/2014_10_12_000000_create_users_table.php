38833d12f548b91e9f91779817c40a5647942cfb
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-05-01 12:18:58 +0900

    途中

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('qualification')->nullable();
+            $table->string('hobby')->nullable();
+            $table->string('introduction')->nullable();
             $table->timestamps();
         });
     }
