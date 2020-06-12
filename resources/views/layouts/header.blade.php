36ff75794b48e48a4b32756b0ce816e4544d10d1
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-06-12 10:52:30 +0900

    ハンバーガーメニュー修正完了

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,11 @@
 @section('header')
 <nav class="navbar navbar-expand-sm navbar-light" style="background-color: #444;">
     <h1 style="margin: 0;"><a class="navbar-brand" href="/" style="color: #fff; font-size: 26px;">{{ config('app.name') }}</a></h1>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" style="border-color: rgba(255,255,255,.3);">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-        <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
             @guest
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('login') }}" style="color: #fff; font-size: 16px;">ログイン</a>
