e6d9a27111a07d64c217e88b1e567d8cd5f3db1b
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-06-18 10:12:41 +0900

    heroku https added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
+        
         Blade::if('auth', function($status){
             return Auth::guard($status)->check();
         });
