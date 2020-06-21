cab9256ef31010cb9a7f7c8c8a06833b21e9d784
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-06-21 17:18:36 +0900

    サイドバー修正完了

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -20,6 +20,7 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
         <div style="width: 100%; overflow: hidden;">
