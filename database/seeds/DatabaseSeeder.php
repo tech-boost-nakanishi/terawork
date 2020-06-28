278c90da14843029f8c247c7cabecbbcf1eb2815
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-06-28 14:06:54 +0900

    ユーザーシーダー追加完了

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-          LanguagesTableSeeder::class,
+            LanguagesTableSeeder::class,
+            UsersTableSeeder::class,
         ]);
     }
 }
