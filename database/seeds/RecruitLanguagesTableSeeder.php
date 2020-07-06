91bbd87a8445815740e20310bb3ed173d27e15b9
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-07-06 09:53:21 +0900

    シーダー修正

--- database/seeds/RecruitLanguagesTableSeeder.php
+++ database/seeds/RecruitLanguagesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class RecruitLanguagesTableSeeder extends Seeder
 {
@@ -15,50 +16,74 @@ class RecruitLanguagesTableSeeder extends Seeder
 	        [
 	          'recruit_id' => 1,
 	          'language_id' => 3,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 2,
 	          'language_id' => 1,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 2,
 	          'language_id' => 2,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 3,
 	          'language_id' => 1,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 3,
 	          'language_id' => 2,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 4,
 	          'language_id' => 4,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 5,
 	          'language_id' => 1,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 5,
 	          'language_id' => 2,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 6,
 	          'language_id' => 1,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 6,
 	          'language_id' => 2,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 7,
 	          'language_id' => 1,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
 	        [
 	          'recruit_id' => 7,
 	          'language_id' => 5,
+	          'created_at' => Carbon::now(),
+	          'updated_at' => Carbon::now(),
 	        ],
         ]);
     }
